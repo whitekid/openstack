a89139e6cdddee56c655e69b88a522021e010927
Choe, Cheng-Dae <whitekid@gmail.com>
2013-04-30 21:01:04 +0900

    add initial role and recipes

---
+++ recipes/base.rb
@@ -0,0 +1,4 @@
+# apt-get install -y ubuntu-cloud-keyring
+# echo deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/grizzly main >> /etc/apt/sources.list.d/grizzly.list
+
+# ntp
---
+++ recipes/default.rb
@@ -0,0 +1,1 @@
+include_recipe "openstack::base"
