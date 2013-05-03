2c4865aca0c71cf13228585ed96e9bcd42037767
Choe, Cheng-Dae <whitekid@gmail.com>
2013-05-03 16:58:30 +0900

    fix typo

--- recipes/rabbitmq.rb
+++ recipes/rabbitmq.rb
@@ -1,5 +1,5 @@
 node.set_unless[:openstack][:rabbitmq][:password] = secure_password
 
 execute 'change rabbitmq password' do
-	command "rabbitmqctl change_password guest %{node[:openstack][:rabbitmq][:password]}"
+	command "rabbitmqctl change_password guest #{node[:openstack][:rabbitmq][:password]}"
 end
